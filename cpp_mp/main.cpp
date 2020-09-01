0df5d53a544ed3b4e6096daef6b63d7d1fdbb718
ghtak <gwonhyeong.tak@gmail.com>
2020-09-01 09:49:00 +0900

    [title] fix formate

--- cpp_mp/main.cpp
+++ cpp_mp/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "type_list.hpp"
+
+#include "mp.hpp"
 
 int main(int argc, char* argv[]) {
     static_assert(
         std::is_same<
             mk_idx_seq<4>::type,
-            idx_seq<0,1,2,3>>::value);
-            
+            idx_seq<0, 1, 2, 3>>::value);
+
     static_assert(
         std::is_same<
             type_list<int, int, int>::rebind<std::tuple>::type,
@@ -14,27 +15,27 @@ int main(int argc, char* argv[]) {
 
     static_assert(
         std::is_same<
-            push_back<type_list<int>,float>::type,
+            push_back<type_list<int>, float>::type,
             type_list<int, float>>::value);
 
     static_assert(
         std::is_same<
-            push_front<type_list<int,int>,float>::type,
+            push_front<type_list<int, int>, float>::type,
             type_list<float, int, int>>::value);
-        
+
     static_assert(
         std::is_same<
-            at<0,type_list<int,float,double>>::type,
+            at<0, type_list<int, float, double>>::type,
             int>::value);
-    
+
     static_assert(
         std::is_same<
-            at<2,type_list<int,float,double>>::type,
+            at<2, type_list<int, float, double>>::type,
             double>::value);
 
     static_assert(
         std::is_same<
-            remove_at<0, type_list<int,float>>::type,
+            remove_at<0, type_list<int, float>>::type,
             type_list<float>>::value);
 
     return 0;
---
+++ cpp_mp/mp.hpp
@@ -0,0 +1,93 @@
+#ifndef __cpp_mp_h__
+#define __cpp_mp_h__
+
+template <std::size_t... S>
+struct idx_seq {
+};
+
+template <std::size_t N, std::size_t... S>
+struct mk_idx_seq : mk_idx_seq<N - 1, N - 1, S...> {
+};
+
+template <std::size_t... S>
+struct mk_idx_seq<0, S...> {
+    using type = idx_seq<S...>;
+};
+
+template <typename... Ts>
+struct type_list {
+    template <template <typename...> class U>
+    struct rebind {
+        using type = U<Ts...>;
+    };
+};
+
+template <typename T, typename... Us>
+struct push_back;
+
+template <template <typename...> class T,
+          typename... Ts,
+          typename... Us>
+struct push_back<T<Ts...>, Us...> {
+    using type = T<Ts..., Us...>;
+};
+
+template <typename T, typename... Us>
+struct push_front;
+
+template <template <typename...> class T,
+          typename... Ts,
+          typename... Us>
+struct push_front<T<Ts...>, Us...> {
+    using type = T<Us..., Ts...>;
+};
+
+template <std::size_t I, typename T>
+struct at;
+
+template <std::size_t I,
+          typename Head,
+          template <typename...> class T,
+          typename... Ts>
+struct at<I, T<Head, Ts...>> {
+    using type = typename at<I - 1, T<Ts...>>::type;
+};
+
+template <typename Head,
+          template <typename...> class T,
+          typename... Ts>
+struct at<0, T<Head, Ts...>> {
+    using type = Head;
+};
+
+template <std::size_t I, typename T, typename U>
+struct _remove_at_impl;
+
+template <std::size_t I,
+          template <typename...> class T,
+          typename Type,
+          typename... Ts,
+          typename... Us>
+struct _remove_at_impl<I, T<Ts...>, T<Type, Us...>> {
+    using type = typename _remove_at_impl<I - 1, T<Ts..., Type>, T<Us...>>::type;
+};
+
+template <template <typename...> class T,
+          typename Type,
+          typename... Ts,
+          typename... Us>
+struct _remove_at_impl<0, T<Ts...>, T<Type, Us...>> {
+    using type = T<Ts..., Us...>;
+};
+
+template <std::size_t I, typename T>
+struct remove_at;
+
+template <std::size_t I,
+          template <typename...> class T,
+          typename... Ts>
+struct remove_at<I, T<Ts...>> {
+    using type = typename _remove_at_impl<I, T<>, T<Ts...>>::type;
+};
+
+#endif

5673e116ae90e1de3ab81a91af3bad7e75fd5b46
gwonhyeong tak <gwonhyeong.tak@gmail.com>
2020-10-12 12:27:31 +0000

    [title] web basic

--- golang_hello_web/main.go
+++ golang_hello_web/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "welcome")
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+func gin_main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
